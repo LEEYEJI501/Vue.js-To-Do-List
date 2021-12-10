2d945e60ed0a8c7425a316fa9c0c22ddd84d468f
KimAeri <kal@bigtorage.com>
2021-12-10 15:44:56 +0900

    Vue.js - TodoList commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+      <todo-header></todo-header>
+      <todo-input v-on:addTodo="addTodo"></todo-input>
+      <todo-list v-bind:propsdata="todoItems" @removeTodo="removeTodo"></todo-list>
+      <todo-footer v-on:removeAll="clearAll"></todo-footer>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addTodo(todoItem) {
+     // 로컬 스토리지에 데이터를 추가하는 로직 
+     localStorage.setItem(todoItem, todoItem);
+     this.todoItems.push(todoItem);
+    },
+    clearAll() {
+            localStorage.clear();
+            this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  created() {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+  },
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput':  TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+
+  body{
+    font-family: 'Noto Sans', sans-serif;
+    text-align: center;
+    background: #f6f6f8;
+  }
+  input{
+    border-style: groove;
+    width: 200px;
+  } 
+  button{
+    border-style: groove;
+  }
+  .shadow{
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
+</style>
